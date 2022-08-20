bd54f09f2c1d4f1fdd4640a66b6269b7b0b86160
susu3131 <anny81327@gmail.com>
2022-08-20 21:02:21 +0800

    Update 2022-08-20T13:02:19.542Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,9 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+  // 手機選單
+  $("#nav-box").click(function (e) {
+    $("#nav-phone").toggle();
+  });
 });
 //# sourceMappingURL=all.js.map

642bdc634dd5daf618507a674817e4230f92e33a
susu3131 <anny81327@gmail.com>
2022-08-21 18:08:10 +0800

    輪播button

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -9,16 +9,75 @@ $(document).ready(function () {
 })
 
 //index-swiper 
+// var swiper = new Swiper(".index-Swiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   centeredSlides: true,
+
+//   breakpoints:{
+//     375:{
+//       slidesPerView: 2.5,
+//       spaceBetween: 20,
+//     },
+//     576:{
+//       slidesPerView: 4,
+//       spaceBetween: 20,
+//     },
+//     767:{
+//       slidesPerView: 8,
+//       spaceBetween: 24,
+//     },
+//     992:{
+//       slidesPerView: 9,
+//       spaceBetween: 24,
+//     }
+
+
+//   }
+//   // 換頁按鈕
+//   // pagination: {
+//   //   el: ".swiper-pagination",
+//   //   clickable: true,
+//   // },
+// });
+
 var swiper = new Swiper(".index-Swiper", {
   slidesPerView: 3,
-  spaceBetween: 8,
-  centeredSlides: true,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+    breakpoints:{
+    375:{
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    576:{
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    992:{
+      slidesPerView: 6,
+      spaceBetween: 24,
+    },
+    1200:{
+      slidesPerView: 8,
+      spaceBetween: 30,
+    }
 
-  // 換頁按鈕
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
+
+
+  }
+
+
 });
 
 

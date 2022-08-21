748844f44dc5d6d58b1844fd07a465fae1856c37
susu3131 <anny81327@gmail.com>
2022-08-21 16:18:09 +0800

    Update 2022-08-21T08:18:07.194Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,5 +5,16 @@ $(document).ready(function () {
   $("#nav-box").click(function (e) {
     $("#nav-phone").toggle();
   });
+}); //index-swiper 
+
+var swiper = new Swiper(".index-Swiper", {
+  slidesPerView: 3,
+  spaceBetween: 8,
+  centeredSlides: true // 換頁按鈕
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
 });
 //# sourceMappingURL=all.js.map
